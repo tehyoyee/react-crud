6c2dfe9699b5579ee9163f555df96fb09453c9f1
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-10-12 02:02:44 +0900

    init

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import { connectDB } from "@/util/database"
+
+export default async function Home() {
+
+  const client = await connectDB
+  const db = client.db('forum')
+  // const db = (await connectDB).db('forum') // 축약가능
+
+  let result = await db.collection('post').find().toArray()  // 모두 가져와서 array로
+  
+  console.log(result)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
